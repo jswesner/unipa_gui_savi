b3cd685b0cbc016dd6a7ec3cbb8d01706155a7bb
jswesner <jeff.wesner@usd.edu>
2024-04-19 10:57:29 -0500

    update april 19

--- code/calculate_diffs.R
+++ code/calculate_diffs.R
@@ -6,40 +6,48 @@ library(readxl)
 library(tidybayes)
 
 # load data
-data_hobo_cleaned = read_csv(file = "data/data_hobo_cleaned.csv")
+data_hobo_cleaned = readRDS(file = "data/data_hobo_cleaned.rds")
 
 # load model
-gam_hobo = readRDS("models/gam_hobo.rds")
+gam_hobo = readRDS("models/gam_hobo_new.rds")
 
 # set grid
-time_grid = tibble(time_s = seq(from = min(data_hobo_cleaned$time_s),
-                                to = max(data_hobo_cleaned$time_s),
+time_grid = tibble(julian_s = seq(from = min(data_hobo_cleaned$julian_s),
+                                to = max(data_hobo_cleaned$julian_s),
                                 length.out = 10)) %>% 
-  expand_grid(name = unique(data_hobo_cleaned$name))
+  expand_grid(month_f = unique(data_hobo_cleaned$month_f)) %>% 
+  left_join(data_hobo_cleaned %>% ungroup %>% distinct(month_f, mean_julian, sd_julian))
 
 # get posteriors over grid
 hobo_posts = time_grid %>% 
-  add_epred_draws(gam_hobo)
+  add_epred_draws(gam_hobo_new) %>% 
+  mutate(julian_s = (julian_s*sd_julian) + mean_julian)
 
 # get posterior of differences across each time/treatment (with help from ChatGTP)
 hobo_post_diffs = hobo_posts%>%
-  arrange(name, time_s, .draw) %>%
-  group_by(name, .draw) %>%
+  arrange(month_f, julian_s, .draw) %>%
+  group_by(month_f, .draw) %>%
   mutate(difference = .epred - lag(.epred),
-         abs_difference = abs(difference)) 
-
-# add original times and plot
-mean_time = unique(data_hobo_cleaned$mean_time)
-sd_time = unique(data_hobo_cleaned$sd_time)
+         abs_difference = abs(difference))
 
 plot_diffs = hobo_post_diffs %>% 
-  mutate(time = (time_s*sd_time) + mean_time) %>% 
-  ggplot(aes(x = time, y = difference)) + 
-  stat_halfeye(aes(group = time_s)) + 
-  facet_wrap(~name) +
+  ggplot(aes(x = julian_s, y = difference)) + 
+  stat_halfeye(aes(group = julian_s)) + 
+  facet_wrap(~month_f, scales = "free_x") +
+  geom_line(data = . %>% filter(.draw <= 100),
+            aes(group = .draw), alpha = 0.2) +
   labs(x = "Time (minutes since start)",
        y = "Change in temperature over interval") +
-  ylim(-20, 20) +
   geom_hline(yintercept = 0)
 
+plot_diffs
+
 ggsave(plot_diffs, file = "plots/plot_diffs.jpg")
+
+
+
+hobo_post_diffs %>% 
+  group_by(julian_s, month_f) %>% 
+  median_qi(difference) %>% print(n = Inf)
+
+
--- code/script_hobo_11_05_2024.R
+++ code/script_hobo_11_05_2024.R
@@ -3,8 +3,9 @@ library(janitor)
 library(lubridate)
 library(brms)
 library(readxl)
+library(tidybayes)
 
-data_Hobo_T_C <- read_excel("data_per_savi/data Hobo T°C.xlsx") %>% 
+data_Hobo_T_C <- read_excel("data_per_savi/data_Hobo_T°C_final.xlsx") %>% 
   clean_names() %>% 
   arrange(date_hour) %>% 
   mutate(time = row_number(),
@@ -17,7 +18,8 @@ data_Hobo_T_C <- read_excel("data_per_savi/data Hobo T°C.xlsx") %>%
          month = month(date_hour),
          day = day(date_hour),
          hour_day = paste0(day, "_", hour),
-         month_day = paste0(month, "_", day)) %>% 
+         month_day = paste0(month, "_", day),
+         julian = julian(date_hour)) %>% 
   group_by(hour_minute) %>% 
   mutate(hour_minute_id = cur_group_id()) %>% 
   ungroup() %>% 
@@ -26,19 +28,26 @@ data_Hobo_T_C <- read_excel("data_per_savi/data Hobo T°C.xlsx") %>%
          hour_minute_s = (hour_minute_id - mean_hour_minute)/sd_hour_minute) %>% 
   pivot_longer(cols = starts_with("t_")) %>%
   mutate(name_day = paste0(name, "_", day)) %>% 
-  mutate(value_s = value/mean(value, na.rm = T))
+  mutate(value_s = value/mean(value, na.rm = T)) %>% 
+  filter(month != 8) %>% 
+  group_by(month) %>% 
+  mutate(mean_julian = mean(julian),
+         sd_julian = sd(julian),
+         julian_s = scale(julian),
+         month_f = as.factor(month))
 
-write_csv(data_Hobo_T_C, file = "data/data_hobo_cleaned.csv")
+saveRDS(data_Hobo_T_C, file = "data/data_hobo_cleaned.rds")
 
-data_Hobo_T_C %>% 
-  ggplot(aes(x = hour_minute_s, y = value_s)) +
-  geom_point(aes(color = as.factor(day))) +
-  geom_line(aes(group = day)) +
-  facet_grid(vars(name))
+test %>% 
+  ggplot(aes(x = julian_s, y = value_s, color = as.factor(month))) +
+  geom_point() +
+  # facet_wrap(~month, scales = "free") +
+  NULL
+  
 
 
 # GAM = generalized additive model
-test = data_Hobo_T_C %>% sample_n(size = 200)
+test = data_Hobo_T_C %>% sample_n(size = 400)
 
 gam_hobo = brm(value ~ s(time_s, by = name_day),
                data = test,
@@ -47,8 +56,30 @@ gam_hobo = brm(value ~ s(time_s, by = name_day),
 
 saveRDS(gam_hobo, file = "models/gam_hobo.rds")
 
+gam_hobo = readRDS(file = "models/gam_hobo.rds")
 
-cond_plot = plot(conditional_effects(gam_hobo), points = T)
 
-cond_plot$`time_s:name_day` + 
-  facet_wrap(~name_day, scales = "free")
+gam_hobo_new = brm(value_s ~ s(julian_s, by = month_f),
+                   data = test,
+                   iter = 1000, chains = 1,
+                   prior = c(prior(normal(1, 0.5), class = "Intercept")))
+
+
+
+cond_plot = plot(conditional_effects(gam_hobo_new), points = T)
+
+
+hobo_epred = tibble(julian_s = seq(min(test$julian_s), 
+                                   max(test$julian_s),
+                                   length.out = 50)) %>% 
+  expand_grid(month_f = unique(test$month_f)) %>% 
+  add_epred_draws(gam_hobo_new, re_formula = NULL)
+
+
+hobo_epred %>% 
+  ggplot(aes(x = julian_s, y = .epred, fill = month_f)) + 
+  stat_lineribbon()
+
+
+
+
